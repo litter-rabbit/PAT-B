5e18233a965ef605ae7da505fcafcb074f817c47
litter-rabbit <709343607@qq.com>
2019-11-06 23:55:28 +0800

    更新

--- src/main/pat/day1/title1016/Main.java
+++ src/main/pat/day1/title1016/Main.java
@@ -1,4 +1,30 @@
 package main.pat.day1.title1016;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s1 = br.readLine().split(" ");
+        String A = s1[0];
+        int da = Integer.parseInt(s1[1]);
+        String B = s1[2];
+        int db = Integer.parseInt(s1[3]);
+        long pa=0;
+        long pb=0;
+        for (char c : A.toCharArray()) {
+            if ((c - '0') == da) {
+                pa = pa * 10 + (c - '0');
+            }
+        }
+        for (char c : B.toCharArray()) {
+            if ((c - '0') == db) {
+                pb = pb * 10 + (c - '0');
+            }
+        }
+        System.out.print(pa + pb);
+    }
+
 }
--- src/main/pat/day1/title1017/Main.java
+++ src/main/pat/day1/title1017/Main.java
@@ -1,4 +1,30 @@
 package main.pat.day1.title1017;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s=br.readLine().split(" ");
+        String a = s[0];
+        int b=Integer.parseInt(s[1]);
+        char[] chars=a.toCharArray();
+        int remain = (chars[0] - '0') / b;
+        if ((remain != 0 && chars.length > 1) || chars.length == 1) {
+            System.out.print(remain);
+        }
+        StringBuilder resutl = new StringBuilder();
+        int temp=(chars[0]-'0')%b;
+        for (int i = 1; i < chars.length; i++) {
+            remain = temp * 10 + (chars[i] - '0');
+            resutl.append(remain / b);
+            temp=remain%b;
+        }
+        System.out.print(resutl.toString());
+        System.out.print(" "+temp);
+        
+
+    }
 }
