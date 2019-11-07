dfc36d7e77b200ecf80918efe66c775ab78a5f23
litter-rabbit <709343607@qq.com>
2019-11-07 20:25:10 +0800

    更新

---
+++ src/main/pat/day2/title1020/Main.java
@@ -0,0 +1,13 @@
+package main.pat.day2.title1020;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
